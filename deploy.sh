edee39978a9b5082421e36fbbbcb989faaa4cb0c
Jean Carlo <jeanca2198@gmail.com>
2020-08-10 19:43:59 -0500

    deply pages

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jeanC-git/vue-multi-select-dual-list-box.git githubpage:gh-pages
+git push -f git@github.com:jeanC-git/vue-multi-select-dual-list-box.git master:gh-pages
 
 cd -
